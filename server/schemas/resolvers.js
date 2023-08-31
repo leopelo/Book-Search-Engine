19a1efc060bcba5637147dc6ffb7dfdafbc98e0a
Leopelo <ludenaleonela@gmail.com>
2023-08-31 18:12:44 -0400

    pushed last changes

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,76 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const resolvers = {
+  Query: {
+    me: (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('books');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+   
+  },
+  Mutation: {
+    login: async (parent, {email, password}) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      if(!user) {
+        return
+      }
+      const token = signToken(user)
+      return { token, user};
+    },
+
+    
+    saveBook: async (parent, {bookId}, context) => {
+        if (context.user) {
+          const book = await Book.create({
+            bookId
+          });
+          await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { books: book._id } }
+          );
+  
+          return book;
+        }
+        throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, {bookId}, context) => {
+      if (context.user) {
+        const book = await Book.findOneAndDelete({
+          _id: bookId,
+
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { books: book._id } }
+        );
+
+        return book;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
+
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+
+startApolloServer();
+
+// `server.js`: Implement the Apollo Server and apply it to the Express server as middleware.
