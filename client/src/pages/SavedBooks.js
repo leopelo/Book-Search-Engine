e6588a439196904832ae3044b15afd8976776dd6
Leopelo <ludenaleonela@gmail.com>
2023-08-31 18:13:26 -0400

    updated save book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -13,7 +13,7 @@ const SavedBooks = () => {
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
-  /*useEffect(() => {
+  useEffect(() => {
     const getUserData = async () => {
       try {
         const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -36,10 +36,10 @@ const SavedBooks = () => {
     };
 
     getUserData();
-  }, [userDataLength]);*/
+  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
+  /*const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -60,7 +60,7 @@ const SavedBooks = () => {
     } catch (err) {
       console.error(err);
     }
-  };
+  };*/
 
   // if data isn't here yet, say so
   if (!userDataLength) {
